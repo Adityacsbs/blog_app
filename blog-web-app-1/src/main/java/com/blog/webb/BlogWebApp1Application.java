a46d341dd85c6a7008edf72f84f5edf965631923
AADITYARAJ SINGH CHOUHAN <94135988+Adityacsbs@users.noreply.github.com>
2024-11-30 22:07:49 +0530

    timepass

--- blog-web-app-1/src/main/java/com/blog/webb/BlogWebApp1Application.java
+++ blog-web-app-1/src/main/java/com/blog/webb/BlogWebApp1Application.java
@@ -9,7 +9,7 @@ public class BlogWebApp1Application {
 	public static void main(String[] args) {
 		SpringApplication.run(BlogWebApp1Application.class, args);
 		//this comment is for when i am try again to push the updated code in git
-		//
+		//aditya
 	}
 
 }
