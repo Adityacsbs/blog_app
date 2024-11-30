f8dc8f4ad3e2d023920499dcb0a6e523dccf8627
AADITYARAJ SINGH CHOUHAN <94135988+Adityacsbs@users.noreply.github.com>
2024-11-30 21:48:51 +0530

    project is started today.

---
+++ blog-web-app-1/src/main/java/com/blog/webb/BlogWebApp1Application.java
@@ -0,0 +1,13 @@
+package com.blog.webb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogWebApp1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogWebApp1Application.class, args);
+	}
+
+}
---
+++ blog-web-app-1/src/test/java/com/blog/webb/BlogWebApp1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blog.webb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogWebApp1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
