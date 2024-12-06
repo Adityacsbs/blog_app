b4bb344d7a09e396ce7e3745f6e09d53c9dc41f4
AADITYARAJ SINGH CHOUHAN <94135988+Adityacsbs@users.noreply.github.com>
2024-12-06 14:08:56 +0530

    error fixid table is creating with hibernate automatically

--- blog-web-app-1/src/main/java/com/blog/webb/entities/User.java
+++ blog-web-app-1/src/main/java/com/blog/webb/entities/User.java
@@ -1,22 +1,22 @@
 package com.blog.webb.entities;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.NoArgsConstructor;
 
 
-@Entity  //@Entity(name="xyz") to change the entity name
-@Table //@Table(name="users") //yeah automatically table banata hai User name par apan ne name change kr diya hai users
+@Entity  
+@Table
 @NoArgsConstructor
 @Getter
 @Setter
-public class User {
+public class User{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.AUTO)
